6f5977a09de26ef8bdbdf13b6b26fe048fdab91e
Leon <ultrafunkamsterdam@users.noreply.github.com>
2017-03-05 03:36:34 +0100

    changed substitution syntax for more compatibility
    OSX users report ${LOGTYPE^^} gave errors.
    Seems like shell and older BASH versions can't handle this substitution

--- functions.sh
+++ functions.sh
@@ -75,7 +75,7 @@ StopSelf()
 	exit 1
 }
 
-CheckOS()
+CheckOS() 
 {
 	distro=$(lsb_release -ds 2>/dev/null || cat /etc/*release 2>/dev/null | head -n1 || uname -om)
 	which yum &>/dev/null && install="yum -y install "
