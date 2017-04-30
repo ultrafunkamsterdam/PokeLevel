e5c6184045168f067558a4b32b7147fc0d61358a
Leon <ultrafunkamsterdam@users.noreply.github.com>
2017-04-30 03:38:32 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -26,18 +26,20 @@ IsInstalled jq gpw
 #echo "XxXxXx"  ## replace by a string that will be the first part of the username so don't use common words
 #}
 
-clear
-echo -e "\n\n"
-Logger start " [ https://github.com/ultrafunkamsterdam ]         "    
-Logger " Total accounts in CSV            : $(NumberTotal)       "
-Logger " Number of available accounts     : $(NumberAvailable)   "
-Logger " Number of not available accounts : $(NumberNotAvailable)"
-Logger " Amount of runtime per account    : $TIME_RUN            "
-echo -e "\n"
-Logger "When accounts become available while the script is running, they will be picked up automatically."
-sleep 7
-Logger "Starting Bots "
-sleep 1
+
+# TODO : Review before removal
+#clear
+#echo -e "\n\n"
+#Logger start " [ https://github.com/ultrafunkamsterdam ]         "    
+#Logger " Total accounts in CSV            : $(NumberTotal)       "
+#Logger " Number of available accounts     : $(NumberAvailable)   "
+#Logger " Number of not available accounts : $(NumberNotAvailable)"
+#Logger " Amount of runtime per account    : $TIME_RUN            "
+#echo -e "\n"
+#Logger "When accounts become available while the script is running, they will be picked up automatically."
+#sleep 7
+#Logger "Starting Bots "
+#sleep 1
 clear
 
 LoopCSV
